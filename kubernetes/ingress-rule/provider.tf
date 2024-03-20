f084998b6f728cc1d7d1705125007cd10d4d37e1
Emmanuel Keziah <keziahema@gmail.com>
2024-03-20 14:25:44 +0000

    Added folder

---
+++ kubernetes/ingress-rule/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+
+    kubernetes = {
+        version = ">= 2.0.0"
+        source = "hashicorp/kubernetes"
+    }
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
